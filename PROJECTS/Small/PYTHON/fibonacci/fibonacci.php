1f683f41fa4785fedc3246cae45c4beb6a9f8d5e
eliottvaldes <eliottvaldes@hotmail.com>
2022-07-01 03:36:52 -0500

    upd -> code identation correction (fibonacci.php)

--- PROJECTS/Small/PYTHON/fibonacci/fibonacci.php
+++ PROJECTS/Small/PYTHON/fibonacci/fibonacci.php
@@ -61,29 +61,29 @@
 
 # recursive function
 def fibonachi_recursive(n):
-if n == 0:
-return 0
-elif n == 1:
-return 1
-else:
-return fibonachi_recursive(n-1) + fibonachi_recursive(n-2)
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return fibonachi_recursive(n-1) + fibonachi_recursive(n-2)
 
 # dynamic programming function
 def fibonachi_dynamic(n):
-fib = [0, 1]
-for i in range(2, n+1):
-fib.append(fib[i-1] + fib[i-2])
-return fib[n]
+    fib = [0, 1]
+    for i in range(2, n+1):
+        fib.append(fib[i-1] + fib[i-2])
+    return fib[n]
 
 # main function
 def main():
-n = int(input("Enter the number of fibonachi numbers: "))
-print("Recursive: ", fibonachi_recursive(n))
-print("Dynamic: ", fibonachi_dynamic(n))
+    n = int(input("Enter the number of fibonachi numbers: "))
+    print("Recursive: ", fibonachi_recursive(n))
+    print("Dynamic: ", fibonachi_dynamic(n))
 
 # call main function
 if __name__ == "__main__":
-main()
+    main()
 
 # output
 # Enter the number of fibonachi numbers: 10
